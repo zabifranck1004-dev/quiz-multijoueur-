8594431a7596bb6a9621cd8b6563ea6e89d4a72f
zabifranck1004-dev <zabifranck1004@gmail.com>
2026-09-06 14:33:03 +0000

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+app.use(express.static('publique'));const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
 app.use(express.static('public'));
 
 let rooms = {};
