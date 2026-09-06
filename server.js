e22bb9964037cb866c5a1ea7bbfaa5285fbccc1d
zabifranck1004-dev <zabifranck1004@gmail.com>
2026-09-06 20:23:20 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,62 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Servir les fichiers statiques du dossier public
+app.use(express.static('public'));
+
+const rooms = {};
+
 io.on('connection', (socket) => {
     console.log(`Un joueur s'est connecté : ${socket.id}`);
 
-    // Création d'un salon (et autres événements...)
-    // ...
+    // Créer un salon
+    socket.on('createRoom', (data) => {
+        const roomCode = Math.random().toString(36).substring(2, 6).toUpperCase();
+        rooms[roomCode] = {
+            host: socket.id,
+            players: [{ id: socket.id, username: data.username, avatar: data.avatar }],
+            gameState: 'waiting',
+            currentLetter: '',
+            answers: {}
+        };
 
-    // Démarrer la partie (par l'hôte)
+        socket.roomCode = roomCode;
+        socket.join(roomCode);
+
+        socket.emit('roomCreated', {
+            roomCode,
+            players: rooms[roomCode].players
+        });
+        console.log(`Salon créé : ${roomCode} par ${data.username}`);
+    });
+
+    // Rejoindre un salon
+    socket.on('joinRoom', (data) => {
+        const room = rooms[data.roomCode];
+        if (!room) {
+            socket.emit('errorMsg', "Ce salon n'existe pas !");
+            return;
+        }
+
+        room.players.push({ id: socket.id, username: data.username, avatar: data.avatar });
+        socket.roomCode = data.roomCode;
+        socket.join(data.roomCode);
+
+        socket.emit('roomJoined', {
+            roomCode: data.roomCode,
+            players: room.players
+        });
+
+        io.to(data.roomCode).emit('updatePlayers', room.players);
+        console.log(`${data.username} a rejoint le salon ${data.roomCode}`);
+    });
+
+    // Démarrer la partie
     socket.on('startGame', () => {
         const roomCode = socket.roomCode;
         const room = rooms[roomCode];
@@ -16,6 +68,7 @@ io.on('connection', (socket) => {
 
         room.gameState = 'playing';
         room.currentLetter = randomLetter;
+        room.answers = {};
 
         io.to(roomCode).emit('gameStarted', {
             letter: randomLetter
@@ -24,7 +77,7 @@ io.on('connection', (socket) => {
         console.log(`Partie lancée dans le salon ${roomCode} - Lettre : ${randomLetter}`);
     });
 
-    // --- REÇU DES RÉPONSES (Doit être ici, au même niveau, PAS DANS startGame) ---
+    // Réception des réponses d'un joueur
     socket.on('submitAnswers', (answers) => {
         const roomCode = socket.roomCode;
         const room = rooms[roomCode];
@@ -48,4 +101,13 @@ io.on('connection', (socket) => {
             socket.emit('waitingForOthers');
         }
     });
+
+    socket.on('disconnect', () => {
+        console.log(`Utilisateur déconnecté : ${socket.id}`);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Serveur en écoute sur le port ${PORT}`);
 });
